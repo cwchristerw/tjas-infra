f89be43871b86046e6c496041ae349c05a205931
Christer Warén <cwchristerw@gmail.com>
2025-09-07 20:50:18 +0300

    Add Maintainer script

---
+++ maintainer.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+underline=`tput smul`
+nounderline=`tput rmul`
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+echo "${bold}"
+echo "
+ .-') _               ('-.      .-')
+(  OO) )             ( OO ).-. ( OO ).
+/     '._      ,--.  / . --. /(_)---\_)
+|'--...__) .-')| ,|  | \-.  \ /    _ |
+'--.  .--'( OO |(_|.-'-'  |  |\  :\` \`.
+   |  |   | \`-'|  | \| |_.'  | '..\`''.)
+   |  |   ,--. |  |  |  .-.  |.-._)   \\
+   |  |   |  '-'  /  |  | |  |\       /
+   \`--'    \`-----'   \`--' \`--' \`-----'
+"
+echo "
+PVJJK 1.VOS NIINISALO
+TIETOJÄRJESTELMÄASENTAJIEN INTRA
+MAINTAINER SCRIPT
+"
+echo -n "${normal}"
+
+echo "${bold}PowerDNS Authorative - MySQL Schema${normal}"
+echo "Downloading..."
+curl https://raw.githubusercontent.com/PowerDNS/pdns/refs/heads/master/modules/gmysqlbackend/schema.mysql.sql -o "$PWD/files/powerdns-authorative/schema.mysql.sql" -s
+
+echo -e "\n\n\n"
